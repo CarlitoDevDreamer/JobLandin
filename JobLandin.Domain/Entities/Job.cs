18363498e6d5726699b414e52af46e28aa35e0a6
CarlitoDevDreamer <carpinfkt@hotmail.com>
2024-04-19 15:33:19 +0100

    Foi feita a base  "Boiler Plate" e para Padrão MVC Clean Architecture

---
+++ JobLandin.Domain/Entities/Job.cs
@@ -0,0 +1,12 @@
+﻿namespace JobLandin.Domain.Entities
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public decimal? Salary { get; set; }
+        public DateTime? CreatedAt { get; set; }
+    }
+}
