cf11b5fd9d80570d5507a29e781b308d4c1441a0
CarlitoDevDreamer <carpinfkt@hotmail.com>
2024-05-08 21:05:20 +0100

    Company List READ

---
+++ JobLandin.Web/Controllers/CompanyController.cs
@@ -0,0 +1,24 @@
+﻿using JobLandin.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobLandin.Web.Controllers
+{
+
+public class CompanyController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public CompanyController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    public IActionResult Index()
+    {
+        var companies = _db.Companies.ToList();
+        return View(companies);
+    }
+
+}
+
+}
