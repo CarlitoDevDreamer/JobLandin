ce6f8ae37f8ae1eabd23f69f311208cf19b92577
CarlitoDevDreamer <carpinfkt@hotmail.com>
2024-05-10 16:15:42 +0100

    Common Interfaces Directory

---
+++ JobLandin.Application/Common/Interfaces/ICompanyRepository.cs
@@ -0,0 +1,10 @@
+﻿using JobLandin.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace JobLandin.Application.Common.Interfaces
+{
+    public interface ICompanyRepository
+    {
+        IEnumerable<Company> GetAll(Expression<Func>);
+    }
+}
