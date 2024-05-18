43844dba6b4d1435e66cd65c04ec21951b6158e5
CarlitoDevDreamer <carpinfkt@hotmail.com>
2024-05-18 14:13:55 +0100

    Teste Local vs Remote Branch Name

--- JobLandin.Web/Program.cs
+++ JobLandin.Web/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();//User Roles
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();//User Roles Teste Matias
 
 builder.Services.AddRazorPages();
 
