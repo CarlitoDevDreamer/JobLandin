394a08a5144cd7cc911e4d6e747f0a0c6b7cff2e
CarlitoDevDreamer <carpinfkt@hotmail.com>
2024-05-18 14:22:02 +0100

    Name Conflict Test

--- JobLandin.Web/Program.cs
+++ JobLandin.Web/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();//User Roles Teste Matias
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();//Name conflict???
 
 builder.Services.AddRazorPages();
 
