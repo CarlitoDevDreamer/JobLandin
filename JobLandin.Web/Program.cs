14ef94e122571a5525108d28d0b6339c4c3c4bb3
CarlitoDevDreamer <carpinfkt@hotmail.com>
2024-05-18 14:36:56 +0100

    //Problema de Nomes dos Branches

--- JobLandin.Web/Program.cs
+++ JobLandin.Web/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();//Mudança pra Novo Nome
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();//Problema de Nomes dos Branches
 
 //Add UnitOfWork (acesso aos reposit�rios)
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
