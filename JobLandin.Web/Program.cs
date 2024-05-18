6b37edaf54fe9a9eac154165cc6d66ba5dfa1999
CarlitoDevDreamer <carpinfkt@hotmail.com>
2024-05-18 14:27:52 +0100

    Conflito de Nomes

--- JobLandin.Web/Program.cs
+++ JobLandin.Web/Program.cs
@@ -15,8 +15,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();//Name conflict???
-
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();//Conflito de Nomes
 builder.Services.AddRazorPages();
 
 //Add UnitOfWork (acesso aos reposit�rios)
