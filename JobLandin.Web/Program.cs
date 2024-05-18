7927dd38b2fbcb007858396823ff3f4b12da6e97
CarlitoDevDreamer <carpinfkt@hotmail.com>
2024-05-18 14:32:51 +0100

    //GitHub Manhoso Dois

--- JobLandin.Web/Program.cs
+++ JobLandin.Web/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();//GitHub Manhoso
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();//GitHub Manhoso Dois
 
 //Add UnitOfWork (acesso aos reposit�rios)
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
